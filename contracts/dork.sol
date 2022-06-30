bda78e2f8793c07c5b54684507482a6dc532fdb1
Hemang Mehta <94932809+Hemang28@users.noreply.github.com>
2022-06-30 13:09:14 +0530

    Add files via upload

--- contracts/dork.sol
+++ contracts/dork.sol
@@ -4,11 +4,75 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 //import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
-
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract dork is ERC20PresetMinterPauser{
     constructor() ERC20PresetMinterPauser("dork","DK"){
 
     }
 }
+
+// interface IERC20 {
+//     function totalSupply() external view returns (uint);
+
+//     function balanceOf(address account) external view returns (uint);
+
+//     function transfer(address recipient, uint amount) external returns (bool);
+
+//     function allowance(address owner, address spender) external view returns (uint);
+
+//     function approve(address spender, uint amount) external returns (bool);
+
+//     function transferFrom(
+//         address sender,
+//         address recipient,
+//         uint amount
+//     ) external returns (bool);
+
+//     event Transfer(address indexed from, address indexed to, uint value);
+//     event Approval(address indexed owner, address indexed spender, uint value);
+// }
+
+contract DORK is IERC20{
+
+    string public decimal="0";
+    uint public override totalSupply;
+    address public founder;
+    mapping(address=>uint) public balances;
+    mapping(address=>mapping(address=>uint)) allowed;
+
+    function balanceOf(address tokenOwner) public view override returns(uint balance){
+        return balances[tokenOwner];
+}
+
+    function transfer(address to,uint tokens) public override returns(bool success){
+        require(balances[msg.sender]>=tokens);
+        balances[to]+=tokens; //balances[to]=balances[to]+tokens;
+        balances[msg.sender]-=tokens;
+        emit Transfer(msg.sender,to,tokens);
+        return true;
+}
+
+    function approve(address spender,uint tokens) public override returns(bool success){
+        require(balances[msg.sender]>=tokens);
+        require(tokens>0);
+        allowed[msg.sender][spender]=tokens;
+        emit Approval(msg.sender,spender,tokens);
+        return true;
+}
+
+    function allowance(address tokenOwner,address spender) public view override returns(uint noOfTokens){
+        return allowed[tokenOwner][spender];
+}
+
+    function transferFrom(address from,address to,uint tokens) public override returns(bool success){
+        require(allowed[from][to]>=tokens);
+        require(balances[from]>=tokens);
+        balances[from]-=tokens;
+        balances[to]+=tokens;
+        return true;
+    }   
+}
+
+
 
